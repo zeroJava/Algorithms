3a62967e33941b14fb184000c4d24ccc7fc588fa
zeroJava <zero_2009@hotmail.co.uk>
2016-08-01 22:02:45 +0100

    sherlock pair completed

--- Algorithms/SortHR/SherlockPairs.cs
+++ Algorithms/SortHR/SherlockPairs.cs
@@ -8,39 +8,7 @@ namespace Algorithms.SortHR
 {
     class SherlockPairs
     {
-        public SherlockPairs()
-        {
-            /*int testcase = int.Parse(Console.ReadLine());
-
-            for(int index = 0; index < testcase; index++)
-            {
-                int size = int.Parse(Console.ReadLine());
-                int[] array = Array.ConvertAll<string, int>(Console.ReadLine().Split(' '), int.Parse);
-            }*/
-        }
-
-        private void Execute(int[] array)
-        {
-            if(array.Length == 1)
-            {
-                Console.WriteLine(array.Count());
-            }
-
-            int highest = array.Max();
-            int lowest = array.Min();
-
-            Dictionary<int, int> sumCount = new Dictionary<int, int>();
-            CreateElements(sumCount, lowest, highest); // https://www.youtube.com/watch?v=TTnvXY82dtM
-        }
-
-        private void CreateElements(Dictionary<int, int> dictionary, int start, int end)
-        {
-            for(int index = start; index <= end; index++)
-            {
-                dictionary.Add(index, 0);
-            }
-        }
-
+        [Obsolete("This method is inefficient. Please use CountPairsMath")]
         public int CountPairs(int[] array)
         {
             int _counter = 0;
@@ -66,9 +34,9 @@ namespace Algorithms.SortHR
             return _counter;
         }
 
-        public int CountPairsMath(int[] array)
+        public long CountPairsMath(int[] array)
         {
-            var _counter = 0;
+            long _counter = 0;
             var _groups = array.GroupBy(x => x);
 
             foreach (var item in _groups)
@@ -78,8 +46,8 @@ namespace Algorithms.SortHR
                     continue;
                 }
 
-                var _size = item.Count();
-                var _multiplyBy = _size - 1;
+                long _size = item.Count();
+                long _multiplyBy = _size - 1;
                 _counter = _counter + _size * _multiplyBy;
             }
 
