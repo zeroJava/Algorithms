c81c62a6c8a00f74d79d6b1aa540819e0e79fc7d
zeroJava <zero_2009@hotmail.co.uk>
2017-10-30 23:20:34 +0000

    queen attack algorithm

---
+++ Algorithms/ImplementationHR/QueensAttack.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.ImplementationHR
+{
+    public class QueensAttack
+    {
+        /* [9]
+         * [8]
+         * [7]
+         * */
+
+        public void Execute()
+        {
+
+        }
+    }
+}
